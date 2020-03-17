c5e252980beba0950de274d651dd515b5681fae8
Jaden Liu <jadentomato@yahoo.com>
2020-03-17 15:54:20 -0700

    Port appname

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const token="Njg5NDk0ODAyNTMzOTc0MTUz.XnDsIQ.iky-DaDb_jgYUy4YMTLcsKzB22g";
 const PREFIX="!";
  
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
 
