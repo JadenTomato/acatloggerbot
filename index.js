329220563544f4a5e5e440627207a76f6501ad8d
Jaden Liu <jadentomato@yahoo.com>
2020-04-30 09:44:43 -0700

    prefix fix v1

--- index.js
+++ index.js
@@ -59,7 +59,9 @@ bot.on("messageDelete", async message =>{
 })
  
 bot.on("message", message=>{
- 
+
+    if(!message.content.startsWith(PREFIX)) return;
+
     let args=message.content.substring(PREFIX.length).split(" ");
  
     switch(args[0]){
